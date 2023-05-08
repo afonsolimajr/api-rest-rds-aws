8ca589f545f78838369161a2640ef147132c2c55
Afonso Lima <afonsolima@gmail.com>
2023-05-07 22:13:20 -0300

    - primeiro commit. subindo codigo fonte inicial

---
+++ src/main/java/dio/api/rds/ApiRestRdsApplication.java
@@ -0,0 +1,13 @@
+package dio.api.rds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestRdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestRdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/api/rds/ApiRestRdsApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.api.rds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestRdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
